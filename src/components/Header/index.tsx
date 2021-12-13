5642b6dd589937fbe38d0ecfef701aa692ff8366
Victor Carli <victorcarlibr@gmail.com>
2021-12-12 21:48:49 -0300

    feat: added component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContant}>
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="logo" width={238} height={26} />
+        </Link>
+      </div>
+    </header>
+  );
 }
