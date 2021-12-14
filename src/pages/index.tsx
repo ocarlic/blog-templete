528a9924563531d82d8e0b8a0d12c35f1003a2be
Victor Carli <victorcarlibr@gmail.com>
2021-12-14 08:16:59 -0300

    feat: home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Link from 'next/link';
 
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +28,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { results } = postsPagination;
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  const formateDate = (first_publication_date): string => {
+    return format(new Date(first_publication_date), 'PP', {
+      locale: ptBR,
+    });
+  };
 
-//   // TODO
-// };
+  return (
+    <main className={commonStyles.wrapper}>
+      <ul className={styles.postContent}>
+        {results.map(post => (
+          <Link href={post.uid}>
+            <li key={post.uid}>
+              <h2>{post.data.title}</h2>
+              <p>{post.data.subtitle}</p>
+
+              <span>
+                <FiCalendar />
+                <time>{formateDate(post.first_publication_date)}</time>
+
+                <FiUser />
+                <p>{post.data.author}</p>
+              </span>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsPagination = await prismic.query(
+    Prismic.Predicates.at('document.type', 'posts')
+  );
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
